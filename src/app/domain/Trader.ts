18adbf7b04c27091c58d7adbf24c5cbc9abdc003
AntonShestakov <melkiades83@gmail.com>
2018-10-01 12:45:07 +0300

    Added methods for Trader

--- src/app/domain/Trader.ts
+++ src/app/domain/Trader.ts
@@ -1,23 +1,64 @@
+import {Trade} from './trade';
+
 export class Trader
 {
-  constructor(private name: string) { }
+  private portfolio: Trade[];
 
-  getName(): string
-  {
+  constructor(private name: string) {
+    this.portfolio  = [];
+  }
+  addToPortfolio(trade: Trade){
+    this.portfolio.push(trade);
+  }
+
+  getPortfolio(): Trade[]{
+    return  this.portfolio;
+  }
+
+  getOpenTrades(): Trade[]{
+
+    return this.portfolio.filter(
+      (element)=>element.get_isOpen()
+    );
+  }
+
+  getClosedTrades(): Trade[]{
+
+    return this.portfolio.filter(
+      (element)=>!element.get_isOpen()
+    );
+  }
+
+   getName(): string  {
     return this.name;
   }
-  getReleasedPnL(): number
-  {
-    return this.getRoundedPnL(0);
+
+  getReleasedPnL(): number  {
+    let releasedPortfolio: Trade[] = this.getClosedTrades();
+    let sum: number = 0;
+
+    releasedPortfolio.forEach((value)=>
+      sum +=  value.getReleasedPnL()
+    )
+    return this.getRoundedPnL(sum);
   }
-  getUnreleasedPnL(): number
-  {
-    return this.getRoundedPnL(0);
+
+  getUnreleasedPnL(): number  {
+    let unReleasedPortfolio: Trade[] = this.getOpenTrades();
+    let sum: number = 0;
+
+    unReleasedPortfolio.forEach((value)=>
+      sum +=  value.getReleasedPnL()
+    )
+
+    return this.getRoundedPnL(sum);
   }
-  getTotalPnL(): number
-  {
+
+
+  getTotalPnL(): number  {
     return this.getRoundedPnL(this.getReleasedPnL() + this.getUnreleasedPnL());
   }
+
   private getRoundedPnL(pnl): number
   {
     return Math.round(pnl * 100 + Number.EPSILON) / 100;
