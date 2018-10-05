de86240a87cf289e2e24d53d0312828ae627d0b7
AntonShestakov <melkiades83@gmail.com>
2018-10-05 12:37:49 +0300

    Implemented Traders Details component

--- src/app/trader-details/trader-details.component.ts
+++ src/app/trader-details/trader-details.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Trader } from '../domain/Trader';
-import {  TradersService  } from '../traders/traders.service';
+import {TradersService  } from '../traders/traders.service';
+import {Trade} from '../domain/trade';
+import {MarketServiceImpl} from '../market/market.service';
+import {FormControl} from '@angular/forms';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-trader-details',
@@ -11,12 +17,40 @@ export class TraderDetailsComponent implements OnInit {
 
   trader: Trader;
 
-  constructor(private traderService: TradersService) {
+  countInput  = new FormControl();
+  symbolInput = new FormControl();
+
+
+  constructor(private traderService: TradersService, private marketService: MarketServiceImpl, private route: ActivatedRoute ) {
     this.trader = new Trader('');
   }
 
   ngOnInit() {
-    this.traderService.getTrader('Oleg').then(trader=>this.trader=trader);
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.tradersService.getTrader(params.get('name')))
+      .subscribe((trader: Trader) => this.trader = trader);
+
+
+    this.countInput.setValue(10);
   }
 
+
+  buyStock()
+  {
+    let trade: Trade =
+      this.marketService.buyStock(this.symbolInput.value,  this.countInput.value);
+
+    if (!trade)
+    {
+      alert(`symbol ${this.symbolInput.value} not found`);
+      return;
+    }
+    this.trader.addToPortfolio(trade);
+    this.symbolInput.setValue('');
+  }
+
+  closeTrade(trade: Trade){
+
+    this.marketService.sellStock(trade);
+  }
 }
